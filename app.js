bac3dded5826c5838fb14b9e50fb988dfe950d8a
Sunny Wong <swong194@gmail.com>
2018-03-02 16:58:06 -0500

    add catalog routes to app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var mongoose = require("mongoose");
 
 var index = require("./routes/index");
 var users = require("./routes/users");
+var catalog = require("./routes/catalog");
 
 var app = express();
 
@@ -33,6 +34,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", index);
 app.use("/users", users);
+app.use("/catalog", catalog);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
