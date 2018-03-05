d7efcbed6255a772e95b14b194e4ef87b93624c4
Sunny Wong <swong194@gmail.com>
2018-03-05 17:42:34 -0500

    reset body margin and padding

--- public/javascripts/index.jsx
+++ public/javascripts/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Root from "./components/root";
 import configureStore from "./store/store";
+import "./index.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
