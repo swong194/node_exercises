9ea9bf9c9d4e5b0c07c53c9d10a0f752851c2b9d
Sunny Wong <swong194@gmail.com>
2018-03-05 13:29:36 -0500

    add store and root component

---
+++ public/javascripts/store/store.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import RootReducer from "../reducers/root_reducer";
+
+const configureStore = (preLoadedState = {}) => {
+  return createStore(RootReducer, preLoadedState, applyMiddleware(thunk));
+};
+
+export default configureStore;
