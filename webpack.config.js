da86f686a831e2655f4056638834200819a59539
Sunny Wong <swong194@gmail.com>
2018-03-05 13:13:58 -0500

    add react/redux, renders hello world

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+  context: __dirname,
+  entry: "./public/javascripts/index.jsx",
+  output: {
+    path: path.resolve(__dirname, "public", "javascripts"),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ["env", "react"]
+        }
+      }
+    ]
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
+};
