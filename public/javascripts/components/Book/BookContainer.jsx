438d8581576e51459ac7d284e445021379a37ac8
Sunny Wong <swong194@gmail.com>
2018-03-06 14:10:36 -0500

    add route to BookContainer in app, fixed typo in BookContainer

--- public/javascripts/components/Book/BookContainer.jsx
+++ public/javascripts/components/Book/BookContainer.jsx
@@ -11,7 +11,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    fetchBooks: dispatch(fetchBooks())
+    fetchBooks: () => dispatch(fetchBooks())
   };
 };
 
--- public/javascripts/components/app.jsx
+++ public/javascripts/components/app.jsx
@@ -3,10 +3,12 @@ import { Route, Switch } from "react-router-dom";
 import Nav from "./Navigation/Nav";
 import CatalogContainer from "./Catalog/CatalogContainer";
 import AuthorContainer from "./Author/AuthorContainer";
+import BookContainer from "./Book/BookContainer";
 
 const App = () => {
   return (
     <Switch>
+      <Route path="/books" component={BookContainer} />
       <Route path="/authors" component={AuthorContainer} />
       <Route path="/" component={CatalogContainer} />
     </Switch>
