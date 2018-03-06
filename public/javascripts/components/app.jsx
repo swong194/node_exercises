ec68eed162c6889092b5718a543817635a182f59
Sunny Wong <swong194@gmail.com>
2018-03-06 13:40:38 -0500

    add route for AuthorComponent in app.jsx

--- public/javascripts/components/app.jsx
+++ public/javascripts/components/app.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Nav from "./Navigation/Nav";
 import CatalogContainer from "./Catalog/CatalogContainer";
+import AuthorContainer from "./Author/AuthorContainer";
 
 const App = () => {
   return (
     <Switch>
+      <Route path="/authors" component={AuthorContainer} />
       <Route path="/" component={CatalogContainer} />
     </Switch>
   );
